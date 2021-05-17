622b93b33525de5cace854788b2adc08f1965f13
John Lafer <jlafer@yahoo.com>
2021-05-17 11:01:40 -0700

    more cleanup

--- plugin-dynamics-serverless/functions/createCase.protected.js
+++ plugin-dynamics-serverless/functions/createCase.protected.js
@@ -1,25 +1,22 @@
 /*
   createCase
   Creates an incident object (displayed as a Case) for the contact specified.
+  
   Required input parameters:
     contactid - the Contact id
-    title - 
+    title - will generate a new Subject object with the given title
     caseorigincode - (1=phone, 2=email, 3=web, see docs for others)
     casetypecode - (1=question, 2=problem, 3=request)
   Optional input parameters:
-    description -
-    productName - the name of an existing, active product in the Sales Hub.
-    subject - (string) will generate a new Subject object with the given Title
+    productName - (optional) the name of an existing, active product in the Sales Hub
+    description - (optional) a case description string
+    subject - (optional) a case subject line
   Additional valid incident properties, if supplied, will be submitted. These are
   documented at https://docs.microsoft.com/en-us/dynamics365/customer-engagement/web-api/incident?view=dynamics-ce-odata-9.
 
   NOTE: if string parameters need URL-encoding, this can be done in Studio using
     liquid like this: {{ widgets.fnDataDip.parsed.suspectStr | url_encode }}
     Such an input parameter will need decoding as done below with title, subject, etc.
-  {"name": "{{widgets.fnFindContact.parsed.firstname}}  {{widgets.fnFindContact.parsed.lastname}}",
-  "contactId": "{{widgets.fnFindContact.parsed.contactid}}",
-  "incidentId": "{{widgets.fnCreateCase.parsed.incidentId}}",
-  "ticketNumber": "{{widgets.fnCreateCase.parsed.ticketNumber}}"  }
 */
 const {corsResponse} = require('jlafer-twilio-runtime-util');
 let helpersPath = Runtime.getFunctions()['dynamicsHelpers'].path;
